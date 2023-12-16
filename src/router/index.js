2680166cd095c087252818611cc813a6969bde2d
zhaowei <15083470173@163.com>
2023-12-16 13:56:53 +0800

    使用标签嵌入在线文档

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const router = createRouter({
       component: () => import('../views/About.vue')
     },
     {
+      path: '/demo',
+      name: 'demo',
+      component: () => import('../views/demo.vue')
+    },
+    {
       path: '/course',
       name: 'course',
       redirect: '/course/outline',
---
+++ src/views/demo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <iframe src="https://docs.qq.com/slide/DWWx5YlZwSW5Callo" width="130%" height="500px" frameborder="0"></iframe>
+  </div>
+</template>
